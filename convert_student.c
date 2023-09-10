f39714d7e4151fcf12b60e364f3d56d89caa765a
ryanhamilton <ryan.hamilton@duke.edu>
2023-09-10 23:56:31 +0000

    ...

--- convert_student.c
+++ convert_student.c
@@ -7,7 +7,19 @@ void hexBin (char *src, char *dst, size_t max);
 
 void binHex (char src[], char dst[], size_t max)
 {
-  // YOUR CODE HERE...
+j = 0;
+  for (i = 0; i < sizeOf(src) - 4; i+4){
+	char fourBit[4] = {src[	i],src[i + 1], src[i + 2], src[i + 3]};
+	switch (fourBit){
+		case "0000":
+			dst[j] = '0';
+			j++;
+			break;
+		case "0000":
+                        dst[j] = '0';
+                        j++;
+                        break;
+.
 }
 
 void hexBin(char src[], char dst[], size_t max)
